9175c51db713a2243f3d2825979f7692719cb9e7
DraconView <nemines002@gmail.com>
2021-08-10 23:47:25 -0500

    1147p100821

---
+++ javascript-aprendizaje/Template-Strings.js
@@ -0,0 +1,24 @@
+Template Strings 
+
+jonmircha 
+- una cualidad es que permite hacer saltos de linea e identacion
+  CAP6 jonmircha
+  
+  //Concatenación
+	let saludo = "Hola mi nombre es " + nombre + " " + apellido + ".";
+	console.log(saludo);
+  //Interpolción de variables
+  //Template String
+	let saludo2 = `Hola mi nombre es ${nombre} ${apellido).`;
+	console.log(saludo2);
+	letul = "<ul><li> Primavera</li><li>Verano</li><li>Otoño</
+	li> <li>Invierno</li></ul>";
+
+ada lovecode
+
+Las plantillas de cadena de texto (template strings) son literales de texto que habilitan
+- Expresiones incrustadas (${expresion})
+- Cadenas de texto de más de una línea.
+- Interpolación expresiones.
+- Plantillas con una función de preprocesado (las veremos más adelante).
+- NOTA: para escapar una comilla dentro de una plantilla se usa \
